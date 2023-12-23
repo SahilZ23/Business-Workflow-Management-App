184859639598752aa61e2a30a249be368bfe744e
Sahil Zaveri <99435767+SahilZ23@users.noreply.github.com>
2023-12-23 15:39:34 -0600

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -126,8 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bwmsoln@gmail.com'
-EMAIL_HOST_PASSWORD = 'aushgfjdcpozpcwv'
+EMAIL_HOST_USER = 'EMAIL FROM'
+EMAIL_HOST_PASSWORD = 'APP PASSWORD'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
